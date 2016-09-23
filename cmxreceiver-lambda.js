5d688a7ad8f1e9c2c1776447fcf3305019f9b8b9
Cory Guynn <cory@premiereportal.net>
2016-09-23 22:34:56 +0100

    added dynamodb support. Work in progress

--- cmxreceiver-lambda.js
+++ cmxreceiver-lambda.js
@@ -26,21 +26,15 @@ var validator = process.env.VALIDATOR || "8e0846499d9a3f6c23f7868c4c25b9d6325035
 
 function cmxData(data) {
     console.log("JSON Feed: " + JSON.stringify(data, null, 2));
-    docClient.putItem(params, data);
+    dynamodb.putItem(params, data);
 };
 
 // *********** Storage ******
 
 
-var AWS = require("aws-sdk");
-//var DOC = require("dynamodb-doc");
+var AWS = require('aws-sdk');
+var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
-//AWS.config.update({region: "us-west-1"});
-
-var docClient = new AWS.DynamoDB.DocumentClient();
-
-var params = {};
-params.TableName = "cmxdata";
 
 // helper function to remove empty elements from a string. Required for DynamoDB
 function removeEmptyStringElements(obj) {
@@ -70,9 +64,22 @@ module.exports = api;
 // Meraki asks for us to know the secret
 api.get("/", function (req) {
     console.log("Validator = " + validator);
-    params.Item = {"validation":new Date()};
-    params.TableName = "cmxdata";
-    docClient.put(params, function(err, data) {
+    var datetime = new Date().getTime().toString();
+    var params = {
+      "TableName":"cmxlog",
+      "Item":{
+        "validator": {"S": validator},
+        "message_ts": {"N": datetime },
+        "message_id": {"S": guid() },
+        "message_apistage": {"S": req.stage },
+        "message_resource": {"S": req.resourcePath },
+        "message_method": {"S": req.httpMethod },
+        "message_ip": {"S": req.sourceIp },
+        "message_supplier": {"S": "meraki" },
+        "message_useragent": {"S": req.userAgent = req.userAgent || '99' }
+      }
+    };
+    dynamodb.putItem(params, function(err, data) {
       if (err) console.log("Error committing to DynamoDB: "+ err);
       else console.log("Committed to DynamoDB:"+ data);
     });
@@ -86,10 +93,10 @@ api.get("/", function (req) {
 api.get("/data", function (req) {
     console.log("requesting data from DynamoDB");
     var params = {
-        TableName: "cmxdata"
-    };
+        "TableName": "cmxdata"
+      };
 
-    docClient.scan(params, function(err, data) {
+    dynamodb.scan(params, function(err, data) {
         if (err){
             console.log(JSON.stringify(err, null, 2));
             return err;
@@ -111,20 +118,92 @@ api.post("/", function (req) {
         console.log("Secret verified");
         console.log("JSON Feed: " + JSON.stringify(req.body, null, 2));
 
-        var params = {
-            TableName:"cmxdata",
-            Item:removeEmptyStringElements(req.body)
-        };
-        docClient.put(params, function(err, data) {
-          if (err) console.log("Error committing to DynamoDB: "+ err);
-          else console.log("Committed to DynamoDB:"+ data);
-        });
-    } else {
-        console.log("Secret was invalid");
+        toDB(req);
+        return "OK";
+    }else{
+        console.log("Secret invalid");
+        return "OK"; // maybe a better response on invalid secret
     }
-    return "OK";
 },
 {
   success: { contentType: 'text/plain' },
   error: {code: 403}
 });
+
+
+// ******* Database stuff
+function guid() {
+ function s4() {
+   return Math.floor((1 + Math.random()) * 0x10000)
+     .toString(16)
+     .substring(1);
+ }
+ return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+   s4() + '-' + s4() + s4() + s4();
+}
+
+
+function toDB(event) {
+
+       try{ // error checking
+
+console.log('event: '+JSON.stringify(event, null, '  '));
+//               console.log("context:::::");
+//               console.log(context['resourcePath']);
+//               console.log("END context:::::");
+
+for (i = 0; i < event.body.data.observations.length; i++) {
+   var observation = event.body.data.observations[i];
+   //Do stuff with observation, like sending it directly to ES
+
+   dynamodb.listTables(function(err, data) {
+   });
+   var datetime = new Date().getTime().toString();
+   var apMac = event.body.data.apMac.toString();
+   var apFloors = event.body.data.apFloors.toString();
+
+
+   var params = {
+       "TableName": "cmxdata",
+       "Item" : {
+               "message_ts": {"N": datetime },
+               "message_id": {"S": guid() },
+               "message_apistage": {"S": event.stage },
+               "message_resource": {"S": event.resourcePath },
+               "message_method": {"S": event.httpMethod },
+               "message_ip": {"S": event.sourceIp },
+               "message_supplier": {"S": "meraki" },
+               "message_useragent": {"S": event.userAgent = event.userAgent || '99' }, // 99 -> not sent {"S": event.userAgent },
+           "apMac": {"S": event.body.apMac },
+           "apFloors": {"S": event.body.apFloors },
+           "clientMac": {"S": observation.clientMac.toString() },
+           "seenEpoch": {"N": observation.seenEpoch.toString() },
+           "seenTime": {"S": observation.seenTime.toString() },
+           "rssi": {"N": observation.rssi.toString() },
+           //"ssid": {"S": observation.ssid.toString()},
+           "manufacturer": {"S": observation.manufacturer.toString() },
+           //"os": {"S": observation.os.toString() },
+           "lat": {"N": observation.location.lat.toString() },
+           "lng": {"N": observation.location.lng.toString() },
+           "unc": {"N": observation.location.unc.toString() }
+   }
+   }
+   }
+
+   dynamodb.putItem(params, function(err, body) {
+       if (err) {
+               //context.fail('error','Insert failed: '+err);
+               console.log(err);
+       }
+       else {
+           console.log('DYNOMITE! great success: '+JSON.stringify(body, null, '  '));
+           //context.succeed('SUCCESS');
+       }
+   });
+
+       }catch(e){ // error checking
+
+       console.log(e.stack); // error checking
+   }
+
+};
